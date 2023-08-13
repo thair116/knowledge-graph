1622d070cfdee1e1aa7918657c77f1e179f06a78
Travis Hairfield <travis.hairfield@gmail.com>
2023-08-13 16:51:14 -0700

    clean up translate function

--- amplify/backend/function/openai/src/app.js
+++ amplify/backend/function/openai/src/app.js
@@ -39,39 +39,50 @@ app.use(function (req, res, next) {
   next()
 });
 
-
 app.post('/translate/query', async function (req, res, next) {
-  const { body } = req;
+  const query = req?.body?.query;
+  if (!query) {
+    res.json({ error: 'no query provided in the body' }) // set status code here
+    return
+  }
 
-  console.log('start')
+  const translation = await getTranslation(query);
 
+  res.json({ success: 'done', content: translation })
+  return
+});
+
+app.listen(3000, function () {
+  console.log("App started")
+});
+
+
+// ===========================
+// Logical functions below here
+// ===========================
+const retrieveSecrets = async () => {
+  // get the OPENAI_KEY from SSM
   const { Parameters } = await (new aws.SSM())
     .getParameters({
       Names: ["OPENAI_KEY"].map(secretName => process.env[secretName]),
       WithDecryption: true,
     })
     .promise();
-
-
-  console.log('start2')
-  console.log(Parameters)
-
-
   const result = {};
   Parameters.forEach(item => {
     const key = item.Name.split('AMPLIFY_openai_').pop();
     result[key] = item.Value;
   });
+  return result;
+}
 
-  console.log('start3')
-  console.log(result)
-
-
-  const { OPENAI_KEY } = result
+const getTranslation = async (query) => {
+  const { OPENAI_KEY } = await retrieveSecrets()
 
   const model = "gpt-4"
-  const system = `You are a text-to-code translator, focussed on graph traversal languages.
-  Your job is to convert plain text English queries into the opencypher graph traversal language.
+  const system = `
+      You are a text-to-code translator, focussed on graph traversal languages.
+      Your job is to convert plain text English queries into the opencypher graph traversal language.
       Reason through it in two steps, first by converting it to a short hand form that
       links together the primary entities via their relations. 
       
@@ -93,15 +104,13 @@ app.post('/translate/query', async function (req, res, next) {
       Which Microsoft employees used to work at LinkedIn?
       1. [company = Microsoft] -> employs  ->  ?  -> past work -> [company = LinkedIn]
       2. MATCH (ms:company {company_name: "Microsoft"})<-[rel:employed_at]-(p:person)-[rel2:employed_at]->(li:company {company_name: "LinkedIn"})
-  WHERE rel.end_date IS NULL and rel2.end_date is not null
-  RETURN p.person_id
+         WHERE rel.end_date IS NULL and rel2.end_date is not null
+         RETURN p.person_id
   `
-  const { query }  = body
-  // const parsed = JSON.parse(body)
+  
   const messages = [{ "role": "system", "content": system }, { "role": "user", "content": query }]
-  console.log(query, messages)
 
-  const params_ = {
+  const params = {
     "temperature": 0.4,
     model,
     messages,
@@ -112,55 +121,14 @@ app.post('/translate/query', async function (req, res, next) {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${OPENAI_KEY}`
     },
-    body: JSON.stringify(params_)
+    body: JSON.stringify(params)
   };
 
+  // these 3 lines might error
   const response = await fetch('https://api.openai.com/v1/chat/completions', requestOptions);
   const data = await response.json();
-
-  console.log(data)
-  res.json({ success: 'done', content: data.choices[0].message.content })
-  return
-
-
-  // const cypherQuery = `
-  //       MATCH (n)
-  //       RETURN n
-  //       LIMIT 10;
-  //   `;
-
-  // const url = 'https://harmonic-instance-1.cbycqrjfsmkf.us-west-2.neptune.amazonaws.com:8182/openCypher';
-  // const params = {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/x-www-form-urlencoded'
-  //   },
-  //   body: `query=${encodeURIComponent(cypherQuery)}&queryType=cypher`
-  // };
-
-  // try {
-  //   const response = await fetch(url, params);
-  //   const data = await response.json();
-
-  //   res.json({ success: 'post call succeed!', body: data })
-  //   return
-  // } catch (error) {
-  //   next(err)
-  //   // res.json({success: 'post call succeed!', url: req.url, body: req.body})
-  //   //   return {
-  //   //       statusCode: 500,
-  //   //       body: `Error: ${error.message}`
-  //   //   };
-  // }
-
-
-  // Add your code here
-  res.json({ success: 'sholdnt be here' })
-});
-
-app.listen(3000, function () {
-  console.log("App started")
-});
+  return data.choices[0].message.content
+}
 
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
